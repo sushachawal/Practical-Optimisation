c4f008bada23722e8fcd40d93f5a9f1b4e5cd78f
sushachawal <ssa43@cam.ac.uk>
2018-11-19 14:23:24 +0000

    Task1 now works

--- Task1.m
+++ Task1.m
@@ -1,13 +1,16 @@
 ns = [16, 64, 256, 512, 1024];
-i = 1;
-n = ns(i);
-m = 2*n;
-A = zeros(m,n);
-b = zeros(m,1);
-save(sprintf('A%d', i),'A');
-save(sprintf('b%d', i),'b');
-f = [zeros(n,1); 1];
-At = [-A, ones(m,1);A,ones(m,1)];
-bt = [-b;b];
-options = optimoptions('linprog','Algorithm','dual-simplex');
-[x, fval, exitflag, output] = linprog(f, At, bt, [], [], zeros(size(f)),[], options)
+for i = 1:5
+    n = ns(i);
+    m = 2*n;
+    load(sprintf('Q1Data/A%d.mat', i));
+    load(sprintf('Q1Data/b%d.mat', i));
+    A = eval(sprintf('A%d', i));
+    b = eval(sprintf('b%d', i));
+    f = [zeros(n,1); 1];
+    At = [-A, ones(m,1);A,ones(m,1)];
+    bt = [-b;b];
+    options = optimoptions('linprog','Algorithm','dual-simplex');
+    
+    % Note importance of using -At, -bt since linprogs expression is: Ax <= b
+    [x, fval, exitflag, output] = linprog(f, -At, -bt, [], [], [],[], options)
+end
