fbb1536c13ded6a371e7d4b841d732a9514eb53d
sushachawal <ssa43@cam.ac.uk>
2019-01-02 11:24:25 +0530

    Rearrange file structure and completes code with figures

--- f.m
+++
@@ -1,3 +0,0 @@
-function c = f(A,b,x,eps)
-    c = sum(((A*x - b).^2 + eps^2).^0.5);
-end
--- gradf.m
+++
@@ -1,4 +0,0 @@
-function out = gradf(A,b,x,eps)
-    temp = A*x - b;
-    out = A'*(temp.*(temp.^2 + eps^2).^(-0.5));
-end
